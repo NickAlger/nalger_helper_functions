794a7e13ba5a7193aaef78d4ab975ae56bdbbd74
NickAlger <nalger225@gmail.com>
2020-06-25 15:59:45 -0700

    Update smooth_function.py

--- smooth_function.py
+++ smooth_function.py
@@ -2,38 +2,49 @@ import numpy as np
 import fenics
 import matplotlib.pyplot as plt
 
-n=40
-num_timesteps = 3
-dt = 1e-2
+run_test = True
 
-mesh = fenics.UnitSquareMesh(40,40)
-V = fenics.FunctionSpace(mesh, 'CG', 1)
+class FenicsFunctionSmoother:
+    def __init__(me, function_space_V, smoothing_time=1e-2, num_timesteps=10):
+        me.num_timesteps = num_timesteps
+        me.dt = smoothing_time / num_timesteps
 
-u = fenics.TrialFunction(V)
-v = fenics.TestFunction(V)
+        u = fenics.TrialFunction(function_space_V)
+        v = fenics.TestFunction(function_space_V)
 
-mass_form = u * v * fenics.dx
-stiffness_form = fenics.inner(fenics.grad(u), fenics.grad(v)) * fenics.dx
+        mass_form = u * v * fenics.dx
+        stiffness_form = fenics.inner(fenics.grad(u), fenics.grad(v)) * fenics.dx
 
-M = fenics.assemble(mass_form)
-Z = fenics.assemble(mass_form + dt * stiffness_form)
+        me.M = fenics.assemble(mass_form)
+        Z = fenics.assemble(mass_form + me.dt * stiffness_form)
 
-Z_solver = fenics.LUSolver(Z)
+        me.Z_solver = fenics.LUSolver(Z)
 
-def smooth_function(f):
-    for k in range(num_timesteps):
-        Z_solver.solve(f.vector(), M * f.vector())
+    def smooth(me, function_f):
+        for k in range(me.num_timesteps):
+            me.Z_solver.solve(function_f.vector(), me.M * function_f.vector())
 
+if run_test:
+    n=40
+    smoothing_times = [1e-4, 1e-3, 1e-2]
 
-w = fenics.Function(V)
-w.vector()[:] = np.random.randn(V.dim())
+    mesh = fenics.UnitSquareMesh(40,40)
+    V = fenics.FunctionSpace(mesh, 'CG', 1)
 
-plt.figure()
-fenics.plot(w)
-plt.title('unsmoothed')
+    f = fenics.Function(V)
+    f_vec = np.random.randn(V.dim())
+    f.vector()[:] = f_vec
 
-smooth_function(w)
+    plt.figure()
+    fenics.plot(f)
+    plt.title('unsmoothed')
 
-plt.figure()
-fenics.plot(w)
-plt.title('smoothed')
+    for smoothing_time in smoothing_times:
+        smoother = FenicsFunctionSmoother(V, smoothing_time=smoothing_time)
+
+        f.vector()[:] = f_vec
+        smoother.smooth(f)
+
+        plt.figure()
+        fenics.plot(f)
+        plt.title('smoothing time=' + str(smoothing_time))
