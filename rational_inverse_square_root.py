1ba83e3ed2363b23d7a390766aa780e724963636
NickAlger <nalger225@gmail.com>
2020-09-18 21:16:49 -0700

    rational_inverse_square_root.py test code into jupyter notebook

--- rational_inverse_square_root.py
+++ rational_inverse_square_root.py
@@ -1,8 +1,6 @@
 # Adaptation of Method 3 from Hale, Higham, and Trefethen, Computing f(A)b by contour integrals. SIAM 2008
+import numpy as np
 from scipy.special import *
-
-
-run_test = True # requires numpy
 
 
 def rational_inverse_square_root_weights_and_poles(interval_min_m, interval_max_M, number_of_rational_terms_N):
@@ -27,20 +25,3 @@ def rational_inverse_square_root_weights_and_poles(interval_min_m, interval_max_
     rational_function = lambda zz: np.dot(1. / (zz.reshape((-1,1)) - poles), weights)
     return weights, poles, rational_function
 
-
-if run_test:
-    import numpy as np
-    m = 1e-1
-    M = 1e3
-    num_test_pts = 1e5
-
-    zz = np.logspace(np.log10(m), np.log10(M), num_test_pts)
-    ff = 1. / np.sqrt(zz)
-
-    for N in range(1, 21):
-        ww, pp, rat = rational_inverse_square_root_weights_and_poles(m, M, N)
-        ff_rat = rat(zz)
-
-        max_err = np.max(np.abs(ff - ff_rat))
-        l2_err = np.linalg.norm(ff - ff_rat)
-        print('N=', N, ', max_err=', max_err, ', l2_err=', l2_err)
