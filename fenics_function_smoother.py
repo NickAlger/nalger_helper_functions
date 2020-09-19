9b9aeae0da42b9ea2f334586724e75a4f185769d
NickAlger <nalger225@gmail.com>
2020-09-18 21:21:58 -0700

    Update fenics_function_smoother.py

--- fenics_function_smoother.py
+++ fenics_function_smoother.py
@@ -1,7 +1,4 @@
-import numpy as np
 import fenics
-
-run_test = False
 
 class FenicsFunctionSmoother:
     def __init__(me, function_space_V, smoothing_time=1e-2, num_timesteps=10):
@@ -22,30 +19,3 @@ class FenicsFunctionSmoother:
     def smooth(me, function_f):
         for k in range(me.num_timesteps):
             me.Z_solver.solve(function_f.vector(), me.M * function_f.vector())
-
-if run_test:
-    import matplotlib.pyplot as plt
-
-    n=40
-    smoothing_times = [1e-4, 1e-3, 1e-2]
-
-    mesh = fenics.UnitSquareMesh(40,40)
-    V = fenics.FunctionSpace(mesh, 'CG', 1)
-
-    f = fenics.Function(V)
-    f_vec = np.random.randn(V.dim())
-    f.vector()[:] = f_vec
-
-    plt.figure()
-    fenics.plot(f)
-    plt.title('unsmoothed')
-
-    for smoothing_time in smoothing_times:
-        smoother = FenicsFunctionSmoother(V, smoothing_time=smoothing_time)
-
-        f.vector()[:] = f_vec
-        smoother.smooth(f)
-
-        plt.figure()
-        fenics.plot(f)
-        plt.title('smoothing time=' + str(smoothing_time))
