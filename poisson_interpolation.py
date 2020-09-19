a77e80919b7f2229d014f9a9a2bb06bd5f54d737
NickAlger <nalger225@gmail.com>
2020-09-18 20:03:19 -0700

    Update poisson_interpolation.py

--- poisson_interpolation.py
+++ poisson_interpolation.py
@@ -1,17 +1,16 @@
 import numpy as np
 import fenics
 import scipy.linalg as sla
-from poisson_point_source_solver import NeumannPoissonSolver
+from neumann_poisson_solver import NeumannPoissonSolver
 
-run_test = False
 
 class PoissonSquaredInterpolation:
-    def __init__(me, function_space_V, point_inds_ii=None):
+    def __init__(me, function_space_V, initial_points=None):
         me.V = function_space_V
-        me.N = V.dim()
+        me.N = me.V.dim()
 
-        u_trial = fenics.TrialFunction(V)
-        v_test = fenics.TestFunction(V)
+        u_trial = fenics.TrialFunction(me.V)
+        v_test = fenics.TestFunction(me.V)
 
         mass_form = u_trial * v_test * fenics.dx
         me.M = fenics.assemble(mass_form)
@@ -22,7 +21,7 @@ class PoissonSquaredInterpolation:
         me.solve_neumann_poisson = me.NPPSS.solve
         me.solve_neumann_point_source = me.NPPSS.solve_point_source
 
-        me.ii = list() # point inds
+        me.points = list()
         me.impulse_responses = list()
         me.solve_S = lambda x: np.nan
         me.eta = np.zeros(me.num_pts)
@@ -30,19 +29,15 @@ class PoissonSquaredInterpolation:
         me.smooth_basis = list()
         me.weighting_functions = list()
 
-        if point_inds_ii is not None:
-            me.add_points(point_inds_ii)
+        if initial_points is not None:
+            me.add_points(initial_points)
 
-    def add_points(me, new_point_inds_ii):
-        num_old_pts = me.num_pts
-        num_new_pts = len(new_point_inds_ii)
-
-        me.ii = me.ii + list(new_point_inds_ii)
+    def add_points(me, new_points):
+        me.points = me.points + new_points
 
         new_impulse_responses = list()
-        for k in range(num_new_pts):
-            ind = me.ii[num_old_pts + k]
-            new_impulse_responses.append(me.solve_neumann_point_source(ind).vector())
+        for p in new_points:
+            new_impulse_responses.append(me.solve_neumann_point_source(p, point_type='coords').vector())
         me.impulse_responses = me.impulse_responses + new_impulse_responses
 
         S = np.zeros((me.num_pts, me.num_pts))
@@ -54,9 +49,8 @@ class PoissonSquaredInterpolation:
         me.mu = np.dot(np.ones(me.num_pts), me.eta)
 
         new_smooth_basis_vectors = list()
-        for k in range(num_new_pts):
-            qk = me.M * new_impulse_responses[k]
-            new_smooth_basis_vectors.append(-me.solve_neumann_poisson(qk).vector())
+        for u in new_impulse_responses:
+            new_smooth_basis_vectors.append(-me.solve_neumann_poisson(me.M * u).vector())
         me.smooth_basis = me.smooth_basis + new_smooth_basis_vectors
 
         me.compute_weighting_functions()
@@ -70,7 +64,7 @@ class PoissonSquaredInterpolation:
 
     @property
     def num_pts(me):
-        return len(me.ii)
+        return len(me.points)
 
     def interpolate_values(me, values_at_points_y):
         y = values_at_points_y
@@ -86,68 +80,3 @@ def make_dense_lu_solver(M):
     M_lu, M_pivot = sla.lu_factor(M)
     solve_M = lambda b: sla.lu_solve((M_lu, M_pivot), b)
     return solve_M
-
-
-if run_test:
-    import matplotlib.pyplot as plt
-    from matplotlib_mouse_click import Click
-
-    n = 75
-    num_pts0 = 51
-
-    mesh = fenics.UnitSquareMesh(n, n)
-    V = fenics.FunctionSpace(mesh, 'CG', 2)
-
-    def nearest_ind(q, xx):
-        return np.argmin(np.linalg.norm(q - xx, axis=1))
-
-    def choose_random_mesh_nodes(V, num_pts0):
-        np.random.seed(0)
-        xx0 = np.random.rand(num_pts0, mesh.geometric_dimension()) * 0.7 + 0.15
-        coords = V.tabulate_dof_coordinates()
-        closest_inds = []
-        for k in range(xx0.shape[0]):
-            closest_inds.append(nearest_ind(xx0[k, :], coords))
-
-        point_inds_ii = np.unique(closest_inds)
-        xx = coords[point_inds_ii, :]
-        num_pts = xx.shape[0]
-        return point_inds_ii, xx, num_pts
-
-    point_inds_ii, xx, num_pts = choose_random_mesh_nodes(V, num_pts0)
-    y = np.random.randn(num_pts)
-
-    PSI = PoissonSquaredInterpolation(V, point_inds_ii=point_inds_ii)
-
-    y = np.random.randn(PSI.num_pts)
-    u = PSI.interpolate_values(y)
-
-    u_fct = fenics.Function(V)
-    u_fct.vector().set_local(u)
-
-    plt.figure()
-    c = fenics.plot(u_fct)
-    for k in range(xx.shape[0]):
-        plt.plot(xx[k, 0], xx[k, 1], '.r')
-    plt.colorbar(c)
-    plt.title('interpolation of random data')
-
-    #
-
-    def plot_kth_weighting_function(k):
-        wk = fenics.Function(V)
-        wk.vector().set_local(PSI.weighting_functions[k])
-        fenics.plot(wk)
-        for k in range(xx.shape[0]):
-            plt.plot(xx[k, 0], xx[k, 1], '.r')
-
-    fig = plt.figure()
-    plot_kth_weighting_function(0)
-    plt.title('weighting function (click on dot)')
-
-    def onclick(event, ax):
-        q = np.array([event.xdata, event.ydata])
-        k = nearest_ind(q, xx)
-        plot_kth_weighting_function(k)
-
-    click = Click(plt.gca(), onclick, button=1)
