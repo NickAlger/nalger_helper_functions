3fd1c6a7af1c110186221dc97debbf512718ee59
NickAlger <nalger225@gmail.com>
2021-03-20 20:17:34 -0700

    faster grid evaluation would be helpful. considering using fenicstools project

--- fenics_function_to_regular_grid_interpolator.py
+++ fenics_function_to_regular_grid_interpolator.py
@@ -8,13 +8,19 @@ from point_is_in_ellipsoid import point_is_in_ellipsoid
 
 class FenicsFunctionToRegularGridInterpolator:
     # https://github.com/NickAlger/helper_functions/blob/master/fenics_function_to_regular_grid_interpolator.ipynb
-    def __init__(me, function_space_V):
+    def __init__(me, function_space_V, grid_min, grid_max, grid_shape):
         me.V = function_space_V
+        me.grid_min = grid_min
+        me.grid_max = grid_max
+        me.grid_shape = grid_shape
+
         mesh = me.V.mesh()
         me.d = mesh.geometric_dimension()
         me.bbt = mesh.bounding_box_tree()
         pt_outside_mesh = dl.Point(np.inf * np.ones(mesh.geometric_dimension()))
         me.bad_bbt_entity = me.bbt.compute_first_entity_collision(pt_outside_mesh)
+
+        _, me.all_XX = make_regular_grid(grid_min, grid_max, grid_shape)
 
     def point_is_in_mesh(me, p_numpy):
         p_fenics = dl.Point(p_numpy)
@@ -23,17 +29,15 @@ class FenicsFunctionToRegularGridInterpolator:
         else:
             return True
 
-    def interpolate(me, u_fenics, grid_min, grid_max, grid_shape,
+    def interpolate(me, u_fenics,
                     outside_domain_default_value=0.0, use_extrapolation=False):
         if use_extrapolation:
             u_fenics.set_allow_extrapolation(True)
 
-        _, all_XX = make_regular_grid(grid_min, grid_max, grid_shape)
-
-        U = np.zeros(grid_shape)
-        for ii in range(np.prod(grid_shape)):
-            nd_ind = np.unravel_index(ii, grid_shape)
-            p = np.array([all_XX[k][nd_ind] for k in range(me.d)])
+        U = np.zeros(me.grid_shape)
+        for ii in range(np.prod(me.grid_shape)):
+            nd_ind = np.unravel_index(ii, me.grid_shape)
+            p = np.array([me.all_XX[k][nd_ind] for k in range(me.d)])
 
             if use_extrapolation:
                 U[nd_ind] = u_fenics(dl.Point(p))
@@ -46,7 +50,7 @@ class FenicsFunctionToRegularGridInterpolator:
 
         return U
 
-    def interpolate_within_ellipsoid(me, u_fenics, grid_min, grid_max, grid_shape,
+    def interpolate_within_ellipsoid(me, u_fenics,
                                      mu, Sigma, tau,
                                      outside_domain_default_value=0.0,
                                      inside_domain_default_value=0.0,
@@ -54,12 +58,10 @@ class FenicsFunctionToRegularGridInterpolator:
         if use_extrapolation:
             u_fenics.set_allow_extrapolation(True)
 
-        _, all_XX = make_regular_grid(grid_min, grid_max, grid_shape)
-
-        U = np.zeros(grid_shape)
-        for ii in range(np.prod(grid_shape)):
-            nd_ind = np.unravel_index(ii, grid_shape)
-            p = np.array([all_XX[k][nd_ind] for k in range(me.d)])
+        U = np.zeros(me.grid_shape)
+        for ii in range(np.prod(me.grid_shape)):
+            nd_ind = np.unravel_index(ii, me.grid_shape)
+            p = np.array([me.all_XX[k][nd_ind] for k in range(me.d)])
 
             if me.point_is_in_mesh(p):
                 if point_is_in_ellipsoid(p, mu, Sigma, tau):  # inside mesh, inside ellipsoid
