ef48f2a6fadb5621a6f0a7d67dc5770883b5afe1
NickAlger <nalger225@gmail.com>
2021-05-06 21:11:32 -0700

    ellipsoid_intersection_test fix bug res.fun vs res.x

--- nalger_helper_functions/ellipsoid_intersection_test.py
+++ nalger_helper_functions/ellipsoid_intersection_test.py
@@ -8,7 +8,7 @@ def ellipsoid_intersection_test(Sigma_A, Sigma_B, mu_A, mu_B, tau):
     res = minimize_scalar(ellipsoid_K_function,
                           bracket=[0.0, 0.5, 1.0],
                           args=(lambdas, v_squared, tau))
-    return np.bool_(res.x >= 0)
+    return np.bool_(res.fun[0] >= 0)
 
 
 def ellipsoid_intersection_test_helper(Sigma_A, Sigma_B, mu_A, mu_B):
