ef51c1aecb010b169caeb9cd0efa20d9160c80fa
NickAlger <nalger225@gmail.com>
2021-06-10 02:46:13 -0700

    fix bug in csr_scipy2fenics

--- nalger_helper_functions/csr_scipy2fenics.py
+++ nalger_helper_functions/csr_scipy2fenics.py
@@ -5,6 +5,6 @@ from petsc4py import PETSc
 def csr_scipy2fenics(A_scipy):
     # Usage:
     #   https://github.com/NickAlger/nalger_helper_functions/blob/master/jupyter_notebooks/csr_conversions.ipynb
-    A_petsc = PETSc.Mat().createAIJ(size=A_scipy.shape, csr=(A_scipy.indptr, A_scipy.indices, A_scipy.array))
+    A_petsc = PETSc.Mat().createAIJ(size=A_scipy.shape, csr=(A_scipy.indptr, A_scipy.indices, A_scipy.data))
     A_fenics = dl.PETScMatrix(A_petsc)
     return A_fenics
