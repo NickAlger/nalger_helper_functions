53544e74e8076df52cd72d730f85a9bb2f032187
NickAlger <nalger225@gmail.com>
2021-10-04 18:22:39 -0500

    added mask function option to eval fenics function on regular grid

--- nalger_helper_functions/eval_fenics_function_on_regular_grid.py
+++ nalger_helper_functions/eval_fenics_function_on_regular_grid.py
@@ -44,7 +44,9 @@ def eval_fenics_function_on_regular_grid_using_direct_evaluation(f, box_min, box
 
 def eval_fenics_function_on_regular_grid_using_boxmesh(f, box_min, box_max, grid_shape,
                                                        outside_mesh_fill_value=None,
-                                                       boundary_reflection=False):
+                                                       boundary_reflection=False,
+                                                       mask_function=None,
+                                                       outside_mask_fill_value=0.0):
     mesh = f.function_space().mesh()
     grid_mesh = box_mesh_nd(box_min, box_max, grid_shape)
     V_grid = dl.FunctionSpace(grid_mesh, 'CG', 1)
@@ -57,15 +59,22 @@ def eval_fenics_function_on_regular_grid_using_boxmesh(f, box_min, box_max, grid
         pp_with_reflection = pp + 2.0 * (pp_projected - pp)
         grid_mesh.coordinates()[:,:] = pp_with_reflection
 
+
+
     f.set_allow_extrapolation(True)
     f_grid = dl.Function(V_grid)
 
     dl.LagrangeInterpolator.interpolate(f_grid, f)
 
+    lexsorted_grid_coords = V_grid.tabulate_dof_coordinates()[lexsort_inds, :]
     F = f_grid.vector()[lexsort_inds].reshape(grid_shape)
 
+    if mask_function is not None:
+        inside_mask = mask_function(lexsorted_grid_coords).reshape(grid_shape)
+        outside_mask = np.logical_not(inside_mask)
+        F[outside_mask] = outside_mask_fill_value
+
     if not (outside_mesh_fill_value is None):
-        lexsorted_grid_coords = V_grid.tabulate_dof_coordinates()[lexsort_inds, :]
         inside_domain = points_inside_mesh(lexsorted_grid_coords, mesh).reshape(grid_shape)
         outside_domain = np.logical_not(inside_domain)
         F[outside_domain] = outside_mesh_fill_value
