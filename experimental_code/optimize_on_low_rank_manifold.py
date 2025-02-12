04f47997dbb8a21721db262a7cee65f6d2f8ecc1
NickAlger <nalger225@gmail.com>
2025-02-12 16:37:51 -0600

    als step still important when changing rank

--- experimental_code/optimize_on_low_rank_manifold.py
+++ experimental_code/optimize_on_low_rank_manifold.py
@@ -1202,7 +1202,27 @@ true_outputs = (Ytrue, Ytrue_r)
 
 #
 
-def als_iter(
+def left_orthogonalize(
+        x: typ.Tuple[
+            jnp.ndarray, # X
+            jnp.ndarray, # Y
+        ],
+) -> typ.Tuple[
+    jnp.ndarray, # Q
+    jnp.ndarray, # Y2
+]:
+    X, Y = x
+    Q, R = jnp.linalg.qr(X, mode='reduced')
+    Y2 = R @ Y
+    return Q, Y2
+
+
+def right_orthogonalize(x):
+    x2_T = left_orthogonalize((x[1].T, x[0].T))
+    return (x2_T[1].T, x2_T[0].T)
+
+
+def sls_iter(
         x0: typ.Tuple[
             jnp.ndarray, # shape=(N,r)
             jnp.ndarray, # shape=(r,M)
@@ -1219,14 +1239,97 @@ def als_iter(
     jnp.ndarray, # shape=(N,r)
     jnp.ndarray, # shape=(r,M)
 ]: # x1
-    X0, Y0 = x0
     Omega, Omega_r = inputs
     Ztrue, Ztrue_r = true_outputs
 
+    X0, Y0 = right_orthogonalize(x0)
+
     X1 = Ztrue @ np.linalg.pinv(Y0 @ Omega)
-    Y1 = np.linalg.pinv(Omega_r @ X0) @ Ztrue_r
-    x1 = (X1, Y1)
-    return x1
+
+    X1b, Y1b = left_orthogonalize((X1, Y0))
+
+    Y2 = np.linalg.pinv(Omega_r @ X1b) @ Ztrue_r
+    x2 = (X1b, Y2)
+    return x2
+
+
+def alscg(
+        x0: typ.Tuple[
+            jnp.ndarray, # shape=(N,r) # X
+            jnp.ndarray, # shape=(r,M) # Y
+        ],
+        compute_x_aux: typ.Callable,                # x                         -> x_aux
+        full_objective_func: typ.Callable,          # x, x_aux                  -> (J, J_aux)
+        full_gradient_func: typ.Callable,           # x, x_aux, J_aux           -> (gX, gY), g_aux
+        full_gn_hessian_matvec_func: typ.Callable,  # x, (pX, pY), x_aux, g_aux -> (HpX, HpY)
+        num_iter: int,
+        cg_rtol: float,
+        cg_max_iter: int = 25,
+) -> typ.Tuple[
+    jnp.ndarray, # shape=(N,r)
+    jnp.ndarray, # shape=(r,M)
+]: # x1
+    x = x0
+
+    for ii in range(num_iter):
+        x = right_orthogonalize(x)
+
+        x_aux = compute_x_aux(x)
+        J, J_aux = full_objective_func(x, x_aux)
+        g, g_aux = full_gradient_func(x, x_aux, J_aux)
+        gX, gY = g
+        norm_gX = jnp.sqrt(jnp.sum(gX)**2)
+        norm_gY = jnp.sqrt(jnp.sum(gY)**2)
+
+        print('iter X:', ii)
+        print('J=', J)
+        print('J_aux=', J_aux)
+        print('norm_gX=', norm_gX)
+        print('norm_gY=', norm_gY)
+
+        gn_hessian_matvec_X = lambda pX: full_gn_hessian_matvec_func(x, (pX, 0.0*x[1]), x_aux, J_aux, g_aux)[0]
+        pX, info = cg_steihaug(gn_hessian_matvec_X, gX, lambda u, v,: u + v, lambda u, c: c * u,
+                               lambda u, v: jnp.sum(u * v), np.inf, cg_rtol, max_iter=cg_max_iter, display=True)
+
+        x = (x[0] + pX, x[1])
+
+        x = left_orthogonalize(x)
+
+        x_aux = compute_x_aux(x)
+        J, J_aux = full_objective_func(x, x_aux)
+        g, g_aux = full_gradient_func(x, x_aux, J_aux)
+        gX, gY = g
+        norm_gX = jnp.sqrt(jnp.sum(gX)**2)
+        norm_gY = jnp.sqrt(jnp.sum(gY)**2)
+
+        print('iter Y:', ii)
+        print('J=', J)
+        print('J_aux=', J_aux)
+        print('norm_gX=', norm_gX)
+        print('norm_gY=', norm_gY)
+
+        alscgY_hessian_matvec = lambda pY: full_gn_hessian_matvec_func(x, (0.0*x[0], pY), x_aux, J_aux, g_aux)[1]
+
+        pY, info = cg_steihaug(alscgY_hessian_matvec, gY, lambda u, v,: u + v, lambda u, c: c * u,
+                               lambda u, v: jnp.sum(u * v), np.inf, cg_rtol, max_iter=cg_max_iter, display=True)
+
+        x = (x[0], x[1] + pY)
+
+    x_aux = compute_x_aux(x)
+    J, J_aux = full_objective_func(x, x_aux)
+    g, g_aux = full_gradient_func(x, x_aux, J_aux)
+    gX, gY = g
+    norm_gX = jnp.sqrt(jnp.sum(gX) ** 2)
+    norm_gY = jnp.sqrt(jnp.sum(gY) ** 2)
+
+    print('final iter:')
+    print('J=', J)
+    print('J_aux=', J_aux)
+    print('norm_gX=', norm_gX)
+    print('norm_gY=', norm_gY)
+
+    return x
+
 
 #
 
@@ -1299,7 +1402,7 @@ def change_rank(
 #
 
 
-rank = 5
+rank = 1
 
 # X0 = jnp.array(np.random.randn(N, rank))
 # Y0 = jnp.array(np.random.randn(rank, M))
@@ -1317,8 +1420,43 @@ Y0 = np.linalg.svd(true_outputs[1])[2][:rank,:]
 # Y0 = np.mean(true_outputs[1], axis=0).reshape((1,-1))
 
 x0 = (X0, Y0)
-# x0 = als_iter(x0, inputs, true_outputs)
-x0 = left_orthogonalize_base(x0)
+
+# x = sls_iter(x0, inputs, true_outputs)
+#
+# A2 = base_to_full(x)
+# computed_err = np.linalg.norm(A2 - A) / np.linalg.norm(A)
+# print('rank=', rank)
+# print('computed_err=', computed_err)
+#
+# U, ss, Vt = np.linalg.svd(A)
+# Ar = U[:, :rank] @ np.diag(ss[:rank]) @ Vt[:rank, :]
+#
+# ideal_err = np.linalg.norm(Ar - A) / np.linalg.norm(A)
+# print('ideal_err=', ideal_err)
+#
+# svals = np.linalg.svd(x[1])[1]
+# print('svals=', svals)
+
+
+x = alscg(
+    x0, compute_x_aux, J_func, g_func, H_matvec_func, 1, 1e-1, cg_max_iter=5,
+)
+
+A2 = base_to_full(x)
+computed_err = np.linalg.norm(A2 - A) / np.linalg.norm(A)
+print('rank=', rank)
+print('computed_err=', computed_err)
+
+U, ss, Vt = np.linalg.svd(A)
+Ar = U[:, :rank] @ np.diag(ss[:rank]) @ Vt[:rank, :]
+
+ideal_err = np.linalg.norm(Ar - A) / np.linalg.norm(A)
+print('ideal_err=', ideal_err)
+
+svals = np.linalg.svd(x[1])[1]
+print('svals=', svals)
+
+x0 = left_orthogonalize_base(x)
 
 J_before, relerr_before = J_func(x0, None)
 
@@ -1356,9 +1494,33 @@ print(relerr_after[1])
 if False:
     rank += 1
 
+    x_aux = compute_x_aux(x)
+
     x0 = change_rank(previous_step, rank, 0.5)
-    x0 = als_iter(x0, inputs, true_outputs) # <-- seems important
-    x0 = left_orthogonalize_base(x0)
+    # prev_x, prev_p, prev_x_aux = previous_step
+    # x0 = change_rank((x, (0.0*prev_p[0], 0.0*prev_p[1]), x_aux), rank, 0.5)
+
+    # x = sls_iter(x0, inputs, true_outputs) # <-- seems important
+
+    x = alscg(
+        x0, compute_x_aux, J_func, g_func, H_matvec_func, 1, 1e-1, cg_max_iter=5,
+    )
+
+    A2 = base_to_full(x)
+    computed_err = np.linalg.norm(A2 - A) / np.linalg.norm(A)
+    print('rank=', rank)
+    print('computed_err=', computed_err)
+
+    U, ss, Vt = np.linalg.svd(A)
+    Ar = U[:, :rank] @ np.diag(ss[:rank]) @ Vt[:rank, :]
+
+    ideal_err = np.linalg.norm(Ar - A) / np.linalg.norm(A)
+    print('ideal_err=', ideal_err)
+
+    svals = np.linalg.svd(x[1])[1]
+    print('svals=', svals)
+
+    x0 = left_orthogonalize_base(x)
 
     J_before, relerr_before = J_func(x0, None)
 
@@ -1390,6 +1552,8 @@ if False:
     print(relerr_after[0])
     print(relerr_after[1])
 
+    # x0 = x
+
 
     #### ALSCG
 
@@ -1399,7 +1563,7 @@ if False:
     Y0 = np.linalg.svd(true_outputs[1])[2][:rank,:]
 
     x0 = (X0, Y0)
-    x0 = als_iter(x0, inputs, true_outputs)
+    x0 = sls_iter(x0, inputs, true_outputs)
     x0 = left_orthogonalize_base(x0)
 
     J_before, relerr_before = J_func(x0, None)
