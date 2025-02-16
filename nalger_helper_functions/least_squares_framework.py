2206b9bcf843f349b29b3b142bf48ac000434c93
NickAlger <nalger225@gmail.com>
2025-02-16 05:10:19 -0600

    least squares framework works

--- nalger_helper_functions/least_squares_framework.py
+++ nalger_helper_functions/least_squares_framework.py
@@ -1,12 +1,6 @@
-import numpy as np
-import jax.numpy as jnp
-import jax
 import typing as typ
-import functools as ft
 
 import nalger_helper_functions.tree_linalg as tla
-
-# jax.config.update("jax_enable_x64", True) # enable double precision
 
 __all__ = [
     'misfit',
@@ -40,25 +34,49 @@ OutputsCoTangent    = typ.TypeVar('OutputsCoTangent')
 Scalar  = typ.TypeVar('Scalar')
 
 
+default_observe = lambda u: u
+default_observe_jvp = lambda u, du: du
+default_observe_vjp = lambda u, dy: dy
+
+def default_loss(y: Outputs, y_true: Outputs) -> typ.Tuple[Scalar, LossAux]:
+    all_num_squared = tla.tree_normsquared_leaves(tla.tree_sub(y_true, y))
+    all_den_squared = tla.tree_normsquared_leaves(y_true)
+    all_relative_squared_errors = tla.tree_div(all_num_squared, all_den_squared)
+    J = 0.5 * tla.tree_sum(all_num_squared)
+    return J, all_relative_squared_errors
+
+def default_loss_vjp(y: Outputs, y_true: Outputs, dJ: Scalar) -> OutputsCoTangent:
+    return tla.tree_scale(y - y_true, dJ)
+
+
+def default_loss_gnhvp(y: Outputs, dy: OutputsTangent) -> OutputsCoTangent:
+    return dy
+
+default_regularization = lambda U: 0.5 * tla.tree_normsquared(U)
+default_regularization_gradient = lambda m: m
+default_regularization_gnhvp = lambda m, dm: dm
+
+
 def misfit(
         m:              Param,
         x:              Inputs,
         y_true:         Outputs, # true observations
         forward_map:    typ.Callable[[Param, Inputs],       typ.Tuple[State,    ForwardAux]],
         observe:        typ.Callable[[State],               Outputs]                        = default_observe,
-        compute_loss:   typ.Callable[[Outputs, Outputs],    typ.Tuple[Scalar,   LossAux]]   = default_compute_loss, # true_obs, obs -> J, J_aux
+        loss:   typ.Callable[[Outputs, Outputs],    typ.Tuple[Scalar,   LossAux]]   = default_loss, # true_obs, obs -> J, J_aux
 ) -> typ.Tuple[
     Scalar, # misfit Jd
     typ.Tuple[
+        ForwardAux,  # Auxiliary data computed when computing the forward map
+        State,
+        Outputs,  # predicted outputs y based on the model m
         LossAux,
-        Outputs, # predicted outputs y based on the model m
-        ForwardAux, # Auxiliary data computed when computing the forward map
     ]
 ]:
     u, f_aux    = forward_map(m, x)
-    y               = observe(u)
-    J, J_aux        = compute_loss(y, y_true)
-    return J, (J_aux, y, f_aux)
+    y           = observe(u)
+    J, J_aux    = loss(y, y_true)
+    return J, (f_aux, u, y, J_aux)
 
 
 def misfit_gradient(
@@ -70,14 +88,14 @@ def misfit_gradient(
         y_true: Outputs,
         forward_map_vjp:    typ.Callable[[Param, Inputs, StateCoTangent, ForwardAux],   typ.Tuple[ParamCoTangent, ForwardVjpAux]],
         observe_vjp:        typ.Callable[[State, OutputsCoTangent],                     StateCoTangent]   = default_observe_vjp,
-        compute_loss_vjp:   typ.Callable[[Outputs, Outputs, Scalar],                    OutputsCoTangent] = default_compute_loss_vjp,
+        loss_vjp:   typ.Callable[[Outputs, Outputs, Scalar],                    OutputsCoTangent] = default_loss_vjp,
 ) -> typ.Tuple[
     ParamCoTangent, # gradient g
     ForwardVjpAux,
 ]:
-    z1: OutputsCoTangent    = compute_loss_vjp(y, y_true, 1.0)
+    z1: OutputsCoTangent    = loss_vjp(y, y_true, 1.0)
     z2: StateCoTangent      = observe_vjp(u, z1)
-    g, fvjp_aux = forward_map_vjp(m, z2, x, f_aux)
+    g, fvjp_aux = forward_map_vjp(m, x,  z2, f_aux)
     return g, fvjp_aux
 
 
@@ -88,13 +106,11 @@ def misfit_gauss_newton_hessian_matvec(
         u:  State,
         f_aux: ForwardAux,
         y: Outputs,
-        y_true: Outputs,
         forward_map_jvp:    typ.Callable[[Param, Inputs, ParamTangent, ForwardAux],   typ.Tuple[StateTangent, ForwardJvpAux]],
         forward_map_vjp:    typ.Callable[[Param, Inputs, StateCoTangent, ForwardAux], typ.Tuple[ParamCoTangent, ForwardVjpAux]],
         observe_jvp:        typ.Callable[[State, StateTangent],     OutputsTangent] = default_observe_jvp,
         observe_vjp:        typ.Callable[[State, OutputsCoTangent], StateCoTangent] = default_observe_vjp,
-        compute_loss_jvp:   typ.Callable[[Outputs, Outputs, OutputsTangent],    Scalar]              = default_compute_loss_jvp,
-        compute_loss_vjp:   typ.Callable[[Outputs, Outputs, Scalar],            OutputsCoTangent]    = default_compute_loss_vjp,
+        loss_gnhvp: typ.Callable[[Outputs, OutputsTangent], OutputsCoTangent] = default_loss_gnhvp,
 ) -> typ.Tuple[
     ParamCoTangent, # H @ dm, Gauss-Newton Hessian vector product
     typ.Tuple[
@@ -102,11 +118,10 @@ def misfit_gauss_newton_hessian_matvec(
         ForwardVjpAux,
     ]
 ]:
-    du, fjvp_aux = forward_map_jvp(m, dm, x, f_aux)
-    dy = observe_jvp(x, du)
-    dJ = compute_loss_jvp(y, y_true, dy)
-    dy2 = compute_loss_vjp(y, y_true, dJ)
-    du2 = observe_vjp(x, dy2)
+    du, fjvp_aux = forward_map_jvp(m, x, dm, f_aux)
+    dy = observe_jvp(u, du)
+    dy2 = loss_gnhvp(y, dy)
+    du2 = observe_vjp(u, dy2)
     Hdm, fvjp_aux = forward_map_vjp(m, x, du2, f_aux)
     return Hdm, (fjvp_aux, fvjp_aux)
 
@@ -118,23 +133,24 @@ def objective(
         forward_map:    typ.Callable[[Param, Inputs], typ.Tuple[State, ForwardAux]],
         a_reg: Scalar,  # regularization parameter
         observe:        typ.Callable[[State],               Outputs]                    = default_observe,
-        compute_loss:   typ.Callable[[Outputs, Outputs],    typ.Tuple[Scalar, LossAux]] = default_compute_loss, # true_obs, obs -> J, J_aux
+        loss:   typ.Callable[[Outputs, Outputs],    typ.Tuple[Scalar, LossAux]] = default_loss, # true_obs, obs -> J, J_aux
         regularization_function: typ.Callable[[Param], Scalar] = default_regularization,
 ) -> typ.Tuple[
     Scalar, # J, objective
     typ.Tuple[
         Scalar, # Jd, misfit component
         Scalar, # Jr, regularization component
-        LossAux,
-        Outputs, # outputs y
         ForwardAux,
+        State,
+        Outputs, # outputs y
+        LossAux,
     ]
 ]:
-    Jd, (Jd_aux, y, f_aux) = misfit(m, x, y_true, forward_map, observe, compute_loss)
+    Jd, (f_aux, u, y, Jd_aux) = misfit(m, x, y_true, forward_map, observe, loss)
     Jr0 = regularization_function(m)
     Jr = tla.tree_scale(Jr0, a_reg)
     J = tla.tree_add(Jd, Jr)
-    return J, (Jd, Jr, Jd_aux, y, f_aux)
+    return J, (Jd, Jr, f_aux, u, y, Jd_aux)
 
 
 def gradient(
@@ -147,7 +163,7 @@ def gradient(
         a_reg: Scalar,
         forward_map_vjp:    typ.Callable[[Param, Inputs, StateCoTangent, ForwardAux],   typ.Tuple[ParamCoTangent, ForwardVjpAux]],
         observe_vjp:        typ.Callable[[State, OutputsCoTangent],     StateCoTangent]   = default_observe_vjp,
-        compute_loss_vjp:   typ.Callable[[Outputs, Outputs, Scalar],    OutputsCoTangent] = default_compute_loss_vjp,
+        loss_vjp:   typ.Callable[[Outputs, Outputs, Scalar],    OutputsCoTangent] = default_loss_vjp,
         regularization_gradient: typ.Callable[[Param], ParamCoTangent] = default_regularization_gradient,
 ) -> typ.Tuple[
     ParamCoTangent, # gradient g
@@ -157,10 +173,10 @@ def gradient(
         ForwardVjpAux,
     ]
 ]:
-    gd, fvjp_aux = misfit_gradient(m, x, u, f_aux, y, y_true, forward_map_vjp, observe_vjp, compute_loss_vjp)
+    gd, fvjp_aux = misfit_gradient(m, x, u, f_aux, y, y_true, forward_map_vjp, observe_vjp, loss_vjp)
     gr0 = regularization_gradient(m)
     gr = tla.tree_scale(gr0, a_reg)
-    g = tla.tree_Add(gd, gr)
+    g = tla.tree_add(gd, gr)
     return g, (gd, gr, fvjp_aux)
 
 
@@ -171,17 +187,13 @@ def gauss_newton_hessian_matvec(
         u: State,
         f_aux: ForwardAux,
         y: Outputs,
-        y_true: Outputs,
-        forward_map_jvp: typ.Callable[
-            [Param, Inputs, ParamTangent, ForwardAux], typ.Tuple[StateTangent, ForwardJvpAux]],
-        forward_map_vjp: typ.Callable[
-            [Param, Inputs, StateCoTangent, ForwardAux], typ.Tuple[ParamCoTangent, ForwardVjpAux]],
+        forward_map_jvp: typ.Callable[[Param, Inputs, ParamTangent, ForwardAux], typ.Tuple[StateTangent, ForwardJvpAux]],
+        forward_map_vjp: typ.Callable[[Param, Inputs, StateCoTangent, ForwardAux], typ.Tuple[ParamCoTangent, ForwardVjpAux]],
         a_reg: Scalar,
         observe_jvp: typ.Callable[[State, StateTangent], OutputsTangent]             = default_observe_jvp,
         observe_vjp: typ.Callable[[State, OutputsCoTangent], StateCoTangent]         = default_observe_vjp,
-        compute_loss_jvp: typ.Callable[[Outputs, Outputs, OutputsTangent], Scalar]   = default_compute_loss_jvp,
-        compute_loss_vjp: typ.Callable[[Outputs, Outputs, Scalar], OutputsCoTangent] = default_compute_loss_vjp,
-        regularization_gauss_newton_hessian_matvec: typ.Callable[[Param, ParamTangent], ParamCoTangent] = default_regularization_gauss_newton_hessian_matvec,
+        loss_gnhvp: typ.Callable[[Outputs, OutputsTangent], OutputsCoTangent] = default_loss_gnhvp,
+        regularization_gnhvp: typ.Callable[[Param, ParamTangent], ParamCoTangent] = default_regularization_gnhvp,
 ) -> typ.Tuple[
     ParamCoTangent, # H @ dm, Gauss-Newton Hessian matvec
     typ.Tuple[
@@ -192,49 +204,13 @@ def gauss_newton_hessian_matvec(
     ]
 ]:
     Hd_dm, (fjvp_aux, fvjp_aux) = misfit_gauss_newton_hessian_matvec(
-        dm, m, x, u, f_aux, y, y_true,
+        dm, m, x, u, f_aux, y,
         forward_map_jvp, forward_map_vjp,
         observe_jvp, observe_vjp,
-        compute_loss_jvp, compute_loss_vjp
+        loss_gnhvp,
     )
-    Hr_dm0 = regularization_gauss_newton_hessian_matvec(m, dm)
+    Hr_dm0 = regularization_gnhvp(m, dm)
     Hr_dm = tla.tree_scale(Hr_dm0, a_reg)
     H_dm = tla.tree_add(Hd_dm, Hr_dm)
     return H_dm, (Hd_dm, Hr_dm, fjvp_aux, fvjp_aux)
 
-
-
-
-
-# regularization_gauss_newton_hessian_matvec: typ.Callable[[Param, ParamTangent], ParamCoTangent] = default_regularization_gauss_newton_hessian_matvec,
-
-
-default_observe = lambda u: u
-default_observe_jvp = lambda u, du: du
-default_observe_vjp = lambda u, dy: dy
-
-def default_compute_loss(y: Outputs, y_true: Outputs) -> typ.Tuple[Scalar, LossAux]:
-    all_num_squared = tla.tree_normsquared_leaves(tla.tree_sub(y_true, y))
-    all_den_squared = tla.tree_normsquared_leaves(y_true)
-    all_relative_squared_errors = tla.tree_div(all_num_squared, all_den_squared)
-    J = 0.5 * tla.tree_sum(all_num_squared)
-    return J, all_relative_squared_errors
-
-def default_compute_loss_jvp(y: Outputs, y_true: Outputs, dy: OutputsTangent) -> Scalar:
-    return tla.tree_dot(y, dy)
-
-def default_compute_loss_vjp(y: Outputs, y_true: Outputs, dJ: Scalar) -> OutputsCoTangent:
-    return tla.tree_scale(y, dJ)
-
-
-default_subtract_obs        = tla.tree_sub
-default_residual_to_misfits = tla.tree_normsquared_leaves
-default_combine_misfits     = lambda m: 0.5 * tla.tree_sum(m)
-
-default_combine_misfits_gradient    = lambda misfits: 0.5 * tla.tree_ones(misfits)
-default_residual_to_misfits_vjp     = lambda residual, misfits_cotangent: tla.tree_mult(tla.tree_scale(misfits_cotangent, 2.0), residual)
-default_obs_operator_vjp            = lambda outputs, obs_cotangent: obs_cotangent
-
-default_regularization = tla.tree_normsquared
-default_regularization_gradient: lambda m: m
-default_regularization_gauss_newton_hessian_matvec: lambda m, dm: dm
---
+++ nalger_helper_functions/test_least_squares_framework.py
@@ -0,0 +1,90 @@
+import numpy as np
+import jax.numpy as jnp
+import jax
+
+from nalger_helper_functions.least_squares_framework import *
+
+jax.config.update("jax_enable_x64", True) # enable double precision
+
+def forward_map(m, x):
+    u0 = jnp.sin(m[0] * x[1]) + x[2]
+    u1 = jnp.cos(x[0]) + x[1] * jnp.exp(m[1]) / x[3]
+    u2 = 3.5 + x[0] + m[0] * m[1] / x[1]
+    return jnp.array([u0, u1, u2]), None
+
+Jac_func = jax.jacobian(forward_map, argnums=0, has_aux=True)
+
+def forward_map_jvp(m, x, dm, f_aux):
+    F, _ = Jac_func(m, x)
+    return F @ dm, None
+
+def forward_map_vjp(m, x, du, f_aux):
+    F, _ = Jac_func(m, x)
+    return F.T @ du, None
+
+x = np.random.randn(4)
+y_true = np.random.randn(3)
+a_reg = np.random.rand()
+
+J_func = lambda m: objective(m, x, y_true, forward_map, a_reg)
+g_func = lambda m, u, f_aux, y: gradient(m, x, u, f_aux, y, y_true, a_reg, forward_map_vjp)
+Hgn_matvec_func = lambda dm, m, u, f_aux, y: gauss_newton_hessian_matvec(dm, m, x, u, f_aux, y, forward_map_jvp, forward_map_vjp, a_reg)
+
+
+m0 = np.random.randn(2)
+J0, (Jd0, Jr0, f_aux0, u0, y0, Jd_aux0) = J_func(m0)
+g0, (gd0, gr0, fvjp_aux0) = g_func(m0, u0, f_aux0, y0)
+
+dm = np.random.randn(2)
+dJ = np.dot(g0, dm)
+dJd = np.dot(gd0, dm)
+dJr = np.dot(gr0, dm)
+H_dm0, (Hd_dm0, Hr_dm0, fjvp_aux0, fvjp_aux0) = Hgn_matvec_func(dm, m0, u0, f_aux0, y0)
+dg = H_dm0
+
+s = 1e-7
+
+m1 = m0 + s * dm
+J1, (Jd1, Jr1, f_aux1, u1, y1, Jd_aux1) = J_func(m1)
+g1, (gd1, gr1, fvjp_aux1) = g_func(m1, u1, f_aux1, y1)
+
+dJ_diff = (J1 - J0) / s
+dJd_diff = (Jd1 - Jd0) / s
+dJr_diff = (Jr1 - Jr0) / s
+err_g = np.abs(dJ - dJ_diff) / np.abs(dJ_diff)
+err_gd = np.abs(dJd - dJd_diff) / np.abs(dJd_diff)
+err_gr = np.abs(dJr - dJr_diff) / np.abs(dJr_diff)
+print('s=', s, ', err_g=', err_g)
+print('s=', s, ', err_gd=', err_gd)
+print('s=', s, ', err_gr=', err_gr)
+
+# Finite check Gauss-Newton Hessian where residual is zero
+
+y_true = forward_map(m0, x)[0]
+
+J_func = lambda m: objective(m, x, y_true, forward_map, a_reg)
+g_func = lambda m, u, f_aux, y: gradient(m, x, u, f_aux, y, y_true, a_reg, forward_map_vjp)
+Hgn_matvec_func = lambda dm, m, u, f_aux, y: gauss_newton_hessian_matvec(dm, m, x, u, f_aux, y, forward_map_jvp, forward_map_vjp, a_reg)
+
+J0, (Jd0, Jr0, f_aux0, u0, y0, Jd_aux0) = J_func(m0)
+g0, (gd0, gr0, fvjp_aux0) = g_func(m0, u0, f_aux0, y0)
+
+dm = np.random.randn(2)
+dg, (dgd, dgr, fjvp_aux0, fvjp_aux0) = Hgn_matvec_func(dm, m0, u0, f_aux0, y0)
+
+s = 1e-7
+
+m1 = m0 + s * dm
+J1, (Jd1, Jr1, f_aux1, u1, y1, Jd_aux1) = J_func(m1)
+g1, (gd1, gr1, fvjp_aux1) = g_func(m1, u1, f_aux1, y1)
+
+dg_diff = (g1 - g0) / s
+dgd_diff = (gd1 - gd0) / s
+dgr_diff = (gr1 - gr0) / s
+err_H = np.linalg.norm(dg - dg_diff) / np.linalg.norm(dg_diff)
+err_Hd = np.linalg.norm(dgd - dgd_diff) / np.linalg.norm(dgd_diff)
+err_Hr = np.linalg.norm(dgr - dgr_diff) / np.linalg.norm(dgr_diff)
+print('s=', s, ', err_H=', err_H)
+print('s=', s, ', err_Hd=', err_Hd)
+print('s=', s, ', err_Hr=', err_Hr)
+
