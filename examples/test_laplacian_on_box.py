81f45bb55fb4825d32fe42eab355f4878a14ab0e
NickAlger <nalger225@gmail.com>
2025-02-18 18:06:38 -0600

    implement periodic BCs for box laplacian

--- examples/test_laplacian_on_box.py
+++ examples/test_laplacian_on_box.py
@@ -4,6 +4,8 @@ import scipy.sparse.linalg as spla
 
 from nalger_helper_functions.laplacian_on_box import *
 
+#### Mix of Dirichlet and Neumann B.C.'s ####
+
 grid_shape = (93, 68)
 box_widths = (2*np.pi, 3*np.pi/2)
 left_bcs = ['Dirichlet', 'Neumann']
@@ -11,17 +13,21 @@ right_bcs = ['Dirichlet', 'Dirichlet']
 
 L = laplacian_nd(grid_shape, box_widths, left_bcs, right_bcs)
 
+# Solve with random function
+
 N = np.prod(grid_shape)
 x = np.random.randn(N)
 y = spla.spsolve(L, x)
 
 plt.figure(figsize=(12,6))
-plt.imshow(y.reshape(grid_shape).T, extent=[0, 2*np.pi, 0, 1*np.pi], origin='lower')
-plt.title('inverse Laplacian times randn')
+plt.imshow(y.reshape(grid_shape).T, extent=[0, box_widths[0], 0, box_widths[1]], origin='lower')
+plt.title('inverse Laplacian times randn\nx BCs: [D, D]\ny BCs: [N, D]')
 plt.colorbar()
 
-xx = np.linspace(0, 2*np.pi, grid_shape[0])
-yy = np.linspace(0, 3 * np.pi / 2, grid_shape[1])
+# Solve with sin(x)cos(y)
+
+xx = np.linspace(0, box_widths[0], grid_shape[0])
+yy = np.linspace(0, box_widths[1], grid_shape[1])
 u = np.outer(np.sin(xx), np.cos(yy)).reshape(-1)
 v = spla.spsolve(L, u)
 
@@ -33,16 +39,16 @@ print('max_h=', max_h, ', err=', err)
 
 plt.figure(figsize=(12,12))
 plt.subplot(2,1,1)
-plt.imshow(u.reshape(grid_shape).T, extent=[0, 2*np.pi, 0, 1*np.pi], origin='lower')
+plt.imshow(u.reshape(grid_shape).T, extent=[0, box_widths[0], 0, box_widths[1]], origin='lower')
 plt.title('sin(x) * cos(y)')
 plt.colorbar()
 
 plt.subplot(2,1,2)
-plt.imshow(v.reshape(grid_shape).T, extent=[0, 2*np.pi, 0, 1*np.pi], origin='lower')
-plt.title('inverse Laplacian times sin(x) * cos(y)')
+plt.imshow(v.reshape(grid_shape).T, extent=[0, box_widths[0], 0, box_widths[1]], origin='lower')
+plt.title('inverse Laplacian times sin(x) * cos(y)\nx BCs: [D, D]\ny BCs: [N, D]')
 plt.colorbar()
 
-#
+# Convergence test:
 
 nx0 = 9
 ny0 = 6
@@ -52,8 +58,8 @@ errs = []
 for grid_shape in grid_shapes:
     L = laplacian_nd(grid_shape, box_widths, left_bcs, right_bcs)
 
-    xx = np.linspace(0, 2 * np.pi, grid_shape[0])
-    yy = np.linspace(0, 3 * np.pi / 2, grid_shape[1])
+    xx = np.linspace(0, box_widths[0], grid_shape[0])
+    yy = np.linspace(0, box_widths[1], grid_shape[1])
     u = np.outer(np.sin(xx), np.cos(yy)).reshape(-1)
     v = spla.spsolve(L, u)
 
@@ -71,5 +77,71 @@ plt.loglog(hh, hh, '--')
 plt.legend(['error', 'h'])
 plt.xlabel('h')
 plt.ylabel('error')
-plt.title('Laplacian error vs. mesh size')
+plt.title('Laplacian error vs. mesh size\nx BCs: [D, D]\ny BCs: [N, D]')
+
+# Periodic B.C.'s
+
+
+#### Periodic B.C.'s ####
+
+grid_shape = (93, 88)
+box_widths = (2*np.pi, 2*np.pi)
+
+L = periodic_laplacian_nd(grid_shape, box_widths)
+
+# Apply to sin(x-1)cos(y-1)
+
+xx = np.linspace(0, box_widths[0], grid_shape[0], endpoint=False)
+yy = np.linspace(0, box_widths[1], grid_shape[1], endpoint=False)
+u = np.outer(np.sin(xx - 1.0), np.cos(yy - 1.0)).reshape(-1)
+v = L @ u
+
+# v = L u = 2 u
+# L u = -(d^2/dx^2 + d^2/dy^2) sin(x)cos(y) = sin(x)cos(x) + sin(x)cos(x) = 2 sin(x)cos(x) = 2 u
+max_h = np.max(1.0 / (np.array(grid_shape)-1))
+err = np.linalg.norm(v - 2*u) / np.linalg.norm(u)
+print('max_h=', max_h, ', err=', err)
+
+plt.figure(figsize=(12,12))
+plt.subplot(2,1,1)
+plt.imshow(u.reshape(grid_shape).T, extent=[0, box_widths[0], 0, box_widths[1]], origin='lower')
+plt.title('sin(x-1) * cos(y-1)')
+plt.colorbar()
+
+plt.subplot(2,1,2)
+plt.imshow(v.reshape(grid_shape).T, extent=[0, box_widths[0], 0, box_widths[1]], origin='lower')
+plt.title('Laplacian times sin(x-1) * cos(y-1)\nPeriodic BCs')
+plt.colorbar()
+
+# Convergence test:
+
+nx0 = 9
+ny0 = 6
+grid_shapes = [(int(nx0*s), int(ny0*s)) for s in np.logspace(np.log10(1), np.log10(20), 10)]
+hh = []
+errs = []
+for grid_shape in grid_shapes:
+    L = periodic_laplacian_nd(grid_shape, box_widths)
+
+    xx = np.linspace(0, box_widths[0], grid_shape[0], endpoint=False)
+    yy = np.linspace(0, box_widths[1], grid_shape[1], endpoint=False)
+    u = np.outer(np.sin(xx-1), np.cos(yy-1)).reshape(-1)
+    v = L @ u
+
+    # v = -L^-1 u = u / 2 since
+    # L u = -(d^2/dx^2 + d^2/dy^2) sin(x)cos(y) = sin(x)cos(x) + sin(x)cos(x) = 2 sin(x)cos(x) = 2 u
+    max_h = np.max(1.0 / (np.array(grid_shape) - 1))
+    err = np.linalg.norm(v - 2*u) / np.linalg.norm(u)
+    print('max_h=', max_h, ', err=', err)
+    hh.append(max_h)
+    errs.append(err)
+
+plt.figure()
+plt.loglog(hh, errs)
+plt.loglog(hh, hh, '--')
+plt.legend(['error', 'h'])
+plt.xlabel('h')
+plt.ylabel('error')
+plt.title('Laplacian error vs. mesh size\nPeriodic BCs')
+
 
--- nalger_helper_functions/laplacian_on_box.py
+++ nalger_helper_functions/laplacian_on_box.py
@@ -1,14 +1,17 @@
 import numpy as np
 import scipy.sparse as sps
 import typing as typ
+import functools as ft
 
 __all__ = [
     'laplacian_1d',
     'dirichlet_laplacian_1d',
     'neumann_laplacian_1d',
+    'periodic_laplacian_1d',
     'laplacian_nd',
     'dirichlet_laplacian_nd',
     'neumann_laplacian_nd',
+    'periodic_laplacian_nd',
 ]
 
 def dirichlet_laplacian_1d(
@@ -34,24 +37,29 @@ def laplacian_1d(
     Note: returns negative of PDE Laplacian. Returned matrix is positive, whereas PDE Laplacian is negative
     '''
     _neumann_labels = {'N', 'n', 'Neumann', 'neumann', 1}
+    _periodic_labels = {'P', 'p', 'Periodic', 'periodic', 1}
+    if (((left_bc in _periodic_labels) and (right_bc not in _periodic_labels)) or
+        ((left_bc not in _periodic_labels) and (right_bc in _periodic_labels))):
+        raise RuntimeWarning('Asked to create Laplacian with one-sided periodicity. Probably not what you want.')
+
     num_cells = num_gridpoints - 1
     h = interval_width / num_cells
-    L = dirichlet_laplacian_1d(num_gridpoints, interval_width)
+    L = dirichlet_laplacian_1d(num_gridpoints, interval_width).tolil()
     if left_bc in _neumann_labels:
         L[0,0] = 1.0 / h**2
+    elif left_bc in _periodic_labels:
+        L[0,-1] = -1.0 / h**2
 
     if right_bc in _neumann_labels:
         L[-1, -1] = 1.0 / h**2
+    elif right_bc in _periodic_labels:
+        L[-1,0] = -1.0 / h**2
 
-    return L
+    return L.tocsr()
 
 
-def neumann_laplacian_1d(
-        num_gridpoints: int, # num_cells + 1
-        interval_width: float,
-) -> sps.csr_matrix:
-    '''-Matrix for -d^2/dx^2 with Neumann boundary conditions'''
-    return laplacian_1d(num_gridpoints, interval_width, 'Neumann', 'Neumann')
+neumann_laplacian_1d = ft.partial(laplacian_1d, left_bc='N', right_bc='N')
+periodic_laplacian_1d = ft.partial(laplacian_1d, left_bc='P', right_bc='P')
 
 
 def laplacian_nd(
@@ -68,14 +76,14 @@ def laplacian_nd(
     N = np.prod(grid_shape)
     L = sps.csr_matrix((N,N))
     for ii in range(d):
-        Li = sps.eye(1).tocsr()
+        Li = sps.eye(1)
         for jj in range(d):
             if jj == ii:
                 X = laplacian_1d(grid_shape[jj], box_widths[jj], left_bcs[jj], right_bcs[jj])
             else:
                 X = sps.eye(grid_shape[jj]).tocsr()
             Li = sps.kron(Li, X, format='csr')
-        L += Li
+        L += Li.tocsr()
     return L
 
 
@@ -83,11 +91,7 @@ def dirichlet_laplacian_nd(
         grid_shape: typ.Sequence[int],      # [num_cells_x+1, num_cells_y+1, ...]
         box_widths: typ.Sequence[float],    # [width_x, width_y, ...]
 ) -> sps.csr_matrix:
-    '''-Matrix for -(d^2/dx^2 + d^2/dy^2 + ...) with Dirichlet B.C.'s.
-    Note: returns negative of PDE Laplacian. Returned matrix is positive, whereas PDE Laplacian is negative
-    '''
-    d = len(grid_shape)
-    bcs = [0 for _ in range(d)]
+    bcs = ['D' for _ in range(len(grid_shape))]
     return laplacian_nd(grid_shape, box_widths, bcs, bcs)
 
 
@@ -95,10 +99,14 @@ def neumann_laplacian_nd(
         grid_shape: typ.Sequence[int],      # [num_cells_x+1, num_cells_y+1, ...]
         box_widths: typ.Sequence[float],    # [width_x, width_y, ...]
 ) -> sps.csr_matrix:
-    '''-Matrix for -(d^2/dx^2 + d^2/dy^2 + ...) with Neumann B.C.'s.
-    Note: returns negative of PDE Laplacian. Returned matrix is positive, whereas PDE Laplacian is negative
-    '''
-    d = len(grid_shape)
-    bcs = [1 for _ in range(d)]
+    bcs = ['N' for _ in range(len(grid_shape))]
+    return laplacian_nd(grid_shape, box_widths, bcs, bcs)
+
+
+def periodic_laplacian_nd(
+        grid_shape: typ.Sequence[int],      # [num_cells_x+1, num_cells_y+1, ...]
+        box_widths: typ.Sequence[float],    # [width_x, width_y, ...]
+) -> sps.csr_matrix:
+    bcs = ['P' for _ in range(len(grid_shape))]
     return laplacian_nd(grid_shape, box_widths, bcs, bcs)
 
