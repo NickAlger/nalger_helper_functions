d57c824c8e73bb486b1f61e637bb5b8af4adced6
NickAlger <nalger225@gmail.com>
2025-02-24 15:43:03 -0600

    figured out basis for tangent space: row sums of dW=0

--- experimental_code/symmetric_symbol_fit.py
+++ experimental_code/symmetric_symbol_fit.py
@@ -10,9 +10,11 @@ import matplotlib.pyplot as plt
 jax.config.update("jax_enable_x64", True) # enable double precision
 
 n = 20
-r = 5
+r = 4
 
 F = jfft.fft(np.eye(n,n), norm='ortho')
+# F = jfft.fft(np.eye(n,n), norm='forward')
+# F = np.linalg.qr(np.random.randn(n,n))[0]
 
 err_fft_h = np.linalg.norm(F.T.conj() @ F - np.eye(n))
 print('err_fft_h=', err_fft_h)
@@ -43,6 +45,60 @@ for ii in range(r):
 
 err_make_A0 = np.linalg.norm(A0 - A0_true) / np.linalg.norm(A0_true)
 print('err_make_A0=', err_make_A0)
+
+#
+
+if False:
+    DW = jnp.einsum('ijk,aj->iak', D, W)
+    DPhi2 = jnp.einsum('ijk,aj->iak', D, Phi*Phi.conj())
+
+    C = jnp.einsum('iaj,jk,kal,lm,man->in', DW, FH, DPhi2, F, DW.conj())
+
+    np.linalg.norm(A - C)
+
+    #
+
+    D4 = np.einsum('ijk,klm->ijlm', D, D)
+
+    D4_true = np.zeros((n,n,n,n))
+    for ii in range(n):
+        D4_true[ii,ii,ii,ii] = 1.0
+
+    np.linalg.norm(D4 - D4_true)
+
+    #
+
+    A = np.random.randn(n,n)
+    iA = np.linalg.inv(A)
+
+    # W = np.einsum('ia,dj,abcd->ijbc', iA, A, D4).reshape((400,400))
+    # W = np.einsum('ia,bj,abcd->icjd', F.T.conj(), F, D4).reshape((400,400))
+    W = np.einsum('ia,dj,abcd->ijbc', F.T.conj(), F, D4).reshape((400,400))
+    W2 = np.einsum('ia,dj,abcd->ijcb', F.T.conj(), F, D4).reshape((400,400))
+    np.linalg.norm(W - W2)
+
+    plt.matshow(W.real)
+    plt.matshow(W.imag)
+
+    Wt = np.einsum('ia,dj,abcd->ijbc', F.T.conj(), F, D4)
+    plt.matshow(Wt[2,:,3,:].real)
+
+    ya = np.random.randn(n)
+    yb = np.random.randn(n)
+
+    z1 = np.einsum('ijbc,b,c', Wt, ya, yb)
+    plt.matshow(z1.real)
+
+    ya2 = ya.copy()
+    yb2 = yb.copy()
+    ya2[1] = 0
+
+    z2 = np.einsum('ijbc,b,c', Wt, ya2, yb2)
+    plt.matshow(z2.real)
+
+    plt.matshow((W.T @ np.random.randn(n*n)).reshape(n,n).real)
+
+#
 
 Q0, R0 = np.linalg.qr(W.T, mode='reduced')
 Q = Q0.T
@@ -126,6 +182,7 @@ for aa in range(r):
         v3 = tx @ tq.T.conj()
         v4 = tx @ tx.T.conj()
         tangent = v1 + v2 + v3 + v4
+        # tangent = v4
         # tangent = tq + tx
         all_tangents.append(tangent)
 
@@ -147,6 +204,7 @@ for aa in range(r):
         v3 = tx @ tq.T.conj()
         v4 = tx @ tx.T.conj()
         tangent = v1 + v2 + v3 + v4
+        # tangent = v4
         # tangent = tq + tx
         all_tangents.append(tangent)
 
@@ -160,6 +218,139 @@ print('dim_tangent_space=', dim_tangent_space)
 
 # (n-r)*r + n*r
 print('(n-1)*(r+1) + 1=', (n-1)*(r+1) + 1)
+print('nr+n-r=', n*r + n - r)
+
+#### Nonpositive tangent space
+
+# Computing dimension of tangent space
+
+n = 20
+r = 4
+
+W = np.random.randn(r,n)
+Phi = np.random.randn(r,n)
+
+all_tangents = []
+k=0
+for aa in range(r):
+    for ii in range(n):
+        dW = np.zeros((r,n))
+
+        dPhi = np.zeros(r*n)
+        dPhi[k] = 1.0
+        dPhi = dPhi.reshape((r,n))
+
+        v1 = np.sum([np.diag(W[ii,:]) @ FH @ np.diag(Phi[ii,:]) @ F @ np.diag(dW[ii,:]) for ii in range(r)], axis=0)
+        v2 = np.sum([np.diag(dW[ii,:]) @ FH @ np.diag(Phi[ii,:]) @ F @ np.diag(W[ii,:]) for ii in range(r)], axis=0)
+        v3 = np.sum([np.diag(W[ii, :]) @ FH @ np.diag(dPhi[ii, :]) @ F @ np.diag(W[ii, :]) for ii in range(r)], axis=0)
+
+        tangent = v1 + v2 + v3
+        all_tangents.append(tangent)
+
+        k += 1
+
+k=0
+for aa in range(r):
+    for ii in range(n):
+        dPhi = np.zeros((r,n))
+
+        dW = np.zeros(r*n)
+        dW[k] = 1.0
+        dW = dW.reshape((r,n))
+
+        v1 = np.sum([np.diag(W[ii,:]) @ FH @ np.diag(Phi[ii,:]) @ F @ np.diag(dW[ii,:]) for ii in range(r)], axis=0)
+        v2 = np.sum([np.diag(dW[ii,:]) @ FH @ np.diag(Phi[ii,:]) @ F @ np.diag(W[ii,:]) for ii in range(r)], axis=0)
+        v3 = np.sum([np.diag(W[ii, :]) @ FH @ np.diag(dPhi[ii, :]) @ F @ np.diag(W[ii, :]) for ii in range(r)], axis=0)
+
+        tangent = v1 + v2 + v3
+        all_tangents.append(tangent)
+
+        k += 1
+
+TTT = np.array(all_tangents)
+ss = np.linalg.svd(TTT.reshape((-1, n*n)))[1]
+
+dim_tangent_space = np.sum(ss > 1e-12)
+print('dim_tangent_space=', dim_tangent_space)
+
+# (n-r)*r + n*r
+print('2*n*r-r=', 2*n*r-r)
+# print('nr+n-r=', n*r + n - r)
+
+#
+
+n = 20
+r = 4
+
+W0 = np.random.randn(r,n)
+Phi0 = np.random.randn(r,n)
+
+W_rowsums = np.sum(W0, axis=1).reshape((-1,1))
+
+W = W0 / W_rowsums
+Phi = Phi0 * W_rowsums**2
+
+A_true  = np.sum([np.diag(W0[ii,:]) @ FH @ np.diag(Phi0[ii,:]) @ F @ np.diag(W0[ii,:]) for ii in range(r)], axis=0)
+A       = np.sum([np.diag(W[ii,:]) @ FH @ np.diag(Phi[ii,:]) @ F @ np.diag(W[ii,:]) for ii in range(r)], axis=0)
+
+err_row_rescaling = np.linalg.norm(A - A_true) / np.linalg.norm(A_true)
+print('err_row_rescaling=', err_row_rescaling)
+
+all_tangents = []
+k=0
+for aa in range(r):
+    for ii in range(n):
+        dW = np.zeros((r,n))
+
+        dPhi = np.zeros(r*n)
+        dPhi[k] = 1.0
+        dPhi = dPhi.reshape((r,n))
+
+        v1 = np.sum([np.diag(W[ii,:]) @ FH @ np.diag(Phi[ii,:]) @ F @ np.diag(dW[ii,:]) for ii in range(r)], axis=0)
+        v2 = np.sum([np.diag(dW[ii,:]) @ FH @ np.diag(Phi[ii,:]) @ F @ np.diag(W[ii,:]) for ii in range(r)], axis=0)
+        v3 = np.sum([np.diag(W[ii, :]) @ FH @ np.diag(dPhi[ii, :]) @ F @ np.diag(W[ii, :]) for ii in range(r)], axis=0)
+
+        tangent = v1 + v2 + v3
+        all_tangents.append(tangent)
+
+        k += 1
+
+
+n_basis = np.linalg.svd(np.eye(n) - 1/n)[0]
+
+k=0
+for aa in range(r):
+    for ii in range(n-1):
+        dPhi = np.zeros((r,n))
+
+        # dW0 = np.zeros(r*n)
+        # dW0[k] = 1.0
+        # dW0 = dW0.reshape((r,n))
+        #
+        # dW_rowsums = np.sum(dW0, axis=1).reshape((-1, 1))
+        #
+        # dW = dW0 - dW_rowsums / n
+
+        dW = np.zeros((r,n))
+        dW[aa,:] = n_basis[:,ii]
+
+        v1 = np.sum([np.diag(W[ii,:]) @ FH @ np.diag(Phi[ii,:]) @ F @ np.diag(dW[ii,:]) for ii in range(r)], axis=0)
+        v2 = np.sum([np.diag(dW[ii,:]) @ FH @ np.diag(Phi[ii,:]) @ F @ np.diag(W[ii,:]) for ii in range(r)], axis=0)
+        v3 = np.sum([np.diag(W[ii, :]) @ FH @ np.diag(dPhi[ii, :]) @ F @ np.diag(W[ii, :]) for ii in range(r)], axis=0)
+
+        tangent = v1 + v2 + v3
+        all_tangents.append(tangent)
+
+        k += 1
+
+TTT = np.array(all_tangents)
+ss = np.linalg.svd(TTT.reshape((-1, n*n)))[1]
+
+dim_tangent_space = np.sum(ss > 1e-12)
+print('dim_tangent_space=', dim_tangent_space)
+
+print('2*n*r-r=', 2*n*r-r)
+
 
 ########
 
