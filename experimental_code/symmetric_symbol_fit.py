52cf8e2219ec9b3e1f25e0d29d3ffa1cb3a5e3c3
NickAlger <nalger225@gmail.com>
2025-02-24 08:22:30 -0600

    experimenting with symmetric symbol manifold fit idea. Orthogonalization and parallel/perp tangent vector splitting works

--- experimental_code/symmetric_symbol_fit.py
+++ experimental_code/symmetric_symbol_fit.py
@@ -34,11 +34,15 @@ FH = F.T.conj()
 W = np.random.randn(r,n)
 Phi = np.random.randn(r,n)
 
-S1 = jnp.einsum('ijk,aj->iak', D, W)
-S2 = jnp.einsum('ijk,aj->iak', D, Phi)
+A0 = jnp.einsum('iaj,jk,kal,lm->im', jnp.einsum('ijk,aj->iak', D, W), FH, jnp.einsum('ijk,aj->iak', D, Phi), F)
+A = A0 @ A0.T.conj()
 
-A0 = jnp.einsum('iaj,jk,kal,lm->im', S1, FH, S2, F)
-A = A0 @ A0.T
+A0_true = np.zeros((n,n), dtype=complex)
+for ii in range(r):
+    A0_true += np.diag(W[ii,:]) @ FH @ np.diag(Phi[ii,:]) @ F
+
+err_make_A0 = np.linalg.norm(A0 - A0_true) / np.linalg.norm(A0_true)
+print('err_make_A0=', err_make_A0)
 
 Q0, R0 = np.linalg.qr(W.T, mode='reduced')
 Q = Q0.T
@@ -46,11 +50,35 @@ R = R0.T
 
 X = jnp.einsum('ab,aj->bj', R, Phi)
 
-T1 = jnp.einsum('ijk,aj->iak', D, Q)
-T2 = jnp.einsum('ijk,aj->iak', D, X)
-
-B0 = jnp.einsum('iaj,jk,kal,lm->im', T1, FH, T2, F)
-B = B0 @ B0.T
+B0 = jnp.einsum('iaj,jk,kal,lm->im', jnp.einsum('ijk,aj->iak', D, Q), FH, jnp.einsum('ijk,aj->iak', D, X), F)
+B = B0 @ B0.T.conj()
 
 err_orth_W = np.linalg.norm(A0 - B0)
 print('err_orth_W=', err_orth_W)
+
+dQA = np.random.randn(*W.shape)
+dXA = np.random.randn(*Phi.shape)
+
+tAq = jnp.einsum('iaj,jk,kal,lm->im', jnp.einsum('ijk,aj->iak', D, dQA), FH, jnp.einsum('ijk,aj->iak', D, X), F)
+tAx = jnp.einsum('iaj,jk,kal,lm->im', jnp.einsum('ijk,aj->iak', D, Q), FH, jnp.einsum('ijk,aj->iak', D, dXA), F)
+tangentA = tAq @ tAq.T.conj() + tAq @ tAx.T.conj() + tAx @ tAq.T.conj() + tAx @ tAx.T.conj()
+
+norm_tangentA = np.linalg.norm(tangentA * tangentA.conj())
+
+C = dQA @ Q.T
+dQA_parallel = C @ Q
+dQA_perp = dQA - dQA_parallel
+
+dQA2 = dQA_perp
+dXA2 = dXA + C.T @ X
+
+tAq2 = jnp.einsum('iaj,jk,kal,lm->im', jnp.einsum('ijk,aj->iak', D, dQA2), FH, jnp.einsum('ijk,aj->iak', D, X), F)
+tAx2 = jnp.einsum('iaj,jk,kal,lm->im', jnp.einsum('ijk,aj->iak', D, Q), FH, jnp.einsum('ijk,aj->iak', D, dXA2), F)
+tangentA2 = tAq2 @ tAq2.T.conj() + tAq2 @ tAx2.T.conj() + tAx2 @ tAq2.T.conj() + tAx2 @ tAx2.T.conj()
+
+err_perp = np.linalg.norm(tangentA - tangentA2) / np.linalg.norm(tangentA)
+print('err_perp=', err_perp)
+
+
+
+
