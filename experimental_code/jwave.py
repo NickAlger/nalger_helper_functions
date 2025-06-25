8fbd70cfff6f03662897607b23c9aea6bad28d2e
NickAlger <nalger225@gmail.com>
2025-06-25 12:01:53 -0500

    jax finite difference wave sim experiments. some instability near left boundary

--- experimental_code/jwave.py
+++ experimental_code/jwave.py
@@ -1,6 +1,7 @@
 import numpy as np
 import jax
 import jax.numpy as jnp
+import typing as typ
 
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
@@ -71,7 +72,7 @@ for nx, ny in zip(all_nx, all_ny):
     hx = xx[1] - xx[0]
     hy = yy[1] - yy[0]
 
-    X, Y = np.meshgrid(xx, yy)
+    X, Y = np.meshgrid(xx, yy, indexing='ij')
 
     U = np.sin(X) * np.cos(Y)
     LU_true = -2.0 * np.sin(X) * np.cos(Y)
@@ -109,22 +110,59 @@ plt.title('LU_true_interior')
 
 # Wave timestepping
 
-def one_timestep(U_cur, U_prev, hx, hy, soundspeed, dt):
-    U_next = 2 * U_cur - U_prev + dt ** 2 * soundspeed ** 2 * laplacian2d(U_cur, hx, hy)
-    return U_next
+RegularGrid = typ.Tuple[
+    jnp.ndarray, # min_point = (xmin, ymin)
+    jnp.ndarray, # max_point = (xmax, ymax)
+    typ.Tuple[int, ...],  # nn = (nx, ny) number of gridpoints per dimension (number of cells+1)
+]
 
-def scan_func(carry, x):
-    U_cur, U_prev, hx, hy, soundspeed, dt = carry
-    U_next = one_timestep(U_cur, U_prev, hx, hy, soundspeed, dt)
-    return (U_next, U_cur, hx, hy, soundspeed, dt), U_next
+def grid_spacings(grid: RegularGrid) -> jnp.ndarray: # hh = (hx, hy)
+    min_point, max_point, nn = grid
+    hh = (max_point - min_point) / (jnp.array(nn) - 1)
+    return hh
 
-soundspeed = 1.1
+TimestepCarry = typ.Tuple[
+    jnp.ndarray, # U_cur,  shape=(nx, ny)
+    jnp.ndarray, # U_prev, shape=(nx, ny)
+    RegularGrid,
+    jnp.ndarray, # soundspeed, shape=(nx, ny)
+    float, # dt, timestep size
+    float, # t_cur, current time
+]
 
-dt = 0.5 * np.minimum(hx, hy) / soundspeed # CFL condition
+source_in_x = jnp.exp(-0.5 * ((X-1.0)**2 + (Y-0.5)**2) / 0.05**2)
 
-U0 = jnp.exp(-0.5 * ((X-1.0)**2 + (Y-0.5)**2) / 0.05**2)
+def one_timestep(carry: TimestepCarry, unused):
+    U_cur, U_prev, grid, soundspeed, dt, t_cur = carry
+    hh = grid_spacings(grid)
+    source = jnp.sin(40*t_cur) * jnp.exp(-0.5 * t_cur**2 / 0.25**2) * source_in_x
 
-U_final, UU = jax.lax.scan(scan_func, (U0, U0, hx, hy, soundspeed, dt), None, length=300)
+    U_next = 2 * U_cur - U_prev + dt ** 2 * soundspeed ** 2 * laplacian2d(U_cur, hh[0], hh[1]) + source
+    t_next = t_cur + dt
+
+    obs_next = U_next
+    carry_next = (U_next, U_cur, grid, soundspeed, dt, t_next)
+
+    return carry_next, obs_next
+
+
+observe_everywhere = lambda U: U
+
+# soundspeed = 1.1 * jnp.ones((nx, ny))
+soundspeed = 1.1 + X + Y
+
+dt = 0.5 * np.minimum(hx, hy) / np.max(soundspeed) # CFL condition
+
+# U0 = jnp.exp(-0.5 * ((X-1.0)**2 + (Y-0.5)**2) / 0.05**2)
+
+U0 = jnp.zeros((nx, ny))
+
+grid = (jnp.array([xmin, ymin]), jnp.array([xmax, ymax]), (nx, ny))
+
+carry_init = (U0, U0, grid, soundspeed, dt, 0.0)
+U_final, UU = jax.lax.scan(one_timestep, carry_init, None, length=400)
+
+# U_final, UU = jax.lax.scan(scan_func, (U0, U0, hx, hy, soundspeed, dt), None, length=300)
 
 vmax = np.max(UU)
 vmin = np.min(UU)
@@ -141,26 +179,27 @@ plt.show()
 
 # inhomogeneous soundspeed
 
-soundspeed = 1.1 + X + Y
+if False:
+    soundspeed = 1.1 + X + Y
 
-dt = 0.5 * np.minimum(hx, hy) / np.max(soundspeed) # CFL condition
+    dt = 0.5 * np.minimum(hx, hy) / np.max(soundspeed) # CFL condition
 
-U0 = jnp.exp(-0.5 * ((X-1.0)**2 + (Y-0.5)**2) / 0.05**2)
+    U0 = jnp.exp(-0.5 * ((X-1.0)**2 + (Y-0.5)**2) / 0.05**2)
 
-U_final, UU = jax.lax.scan(scan_func, (U0, U0, hx, hy, soundspeed, dt), None, length=300)
+    U_final, UU = jax.lax.scan(scan_func, (U0, U0, hx, hy, soundspeed, dt), None, length=300)
 
-vmax = np.max(UU)
-vmin = np.min(UU)
-frames = [] # for storing the generated images
-fig = plt.figure()
-for U in UU:
-    frames.append([plt.imshow(U, origin='lower', extent=(xmin, xmax, ymin, ymax), vmin=vmin, vmax=vmax,
-                              cmap=cm.Greys_r, animated=True)])
+    vmax = np.max(UU)
+    vmin = np.min(UU)
+    frames = [] # for storing the generated images
+    fig = plt.figure()
+    for U in UU:
+        frames.append([plt.imshow(U, origin='lower', extent=(xmin, xmax, ymin, ymax), vmin=vmin, vmax=vmax,
+                                  cmap=cm.Greys_r, animated=True)])
 
-ani = animation.ArtistAnimation(fig, frames, interval=50, blit=True,
-                                repeat_delay=1000)
-# ani.save('wave1.mp4')
-plt.show()
+    ani = animation.ArtistAnimation(fig, frames, interval=50, blit=True,
+                                    repeat_delay=1000)
+    # ani.save('wave1.mp4')
+    plt.show()
 
 
 
